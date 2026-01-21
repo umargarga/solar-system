d627f3323374146b03105ca06594d2e52d4304f8
Umar Yusuf Dayyabu <udygarga@gmail.com>
2026-01-21 13:59:51 +0100

    post planet

--- app.js
+++ app.js
@@ -39,23 +39,19 @@ var dataSchema = new Schema({
 var planetModel = mongoose.model('planets', dataSchema);
 
 
-
-app.post('/planet', async function (req, res) {
-    try {
-        const planetData = await planetModel.findOne({
-            id: req.body.id
-        });
-
-        if (!planetData) {
-            return res.status(404).send("Planet not found. Select a number from 0 - 9");
+app.post('/planet',   function(req, res) {
+   // console.log("Received Planet ID " + req.body.id)
+    planetModel.findOne({
+        id: req.body.id
+    }, function(err, planetData) {
+        if (err) {
+            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
+            res.send("Error in Planet Data")
+        } else {
+            res.send(planetData);
         }
-
-        res.send(planetData);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error in Planet Data");
-    }
-});
+    })
+})
 
 
 app.get('/',   async (req, res) => {
